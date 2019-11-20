45fd2fbb6bbb6b733cf6137a0f25ce68ea940ad1
zhangweiguo <27614330@qq.com>
2019-11-20 14:53:57 +0800

    ##

--- src/utility/chrono.cpp
+++ src/utility/chrono.cpp
@@ -1,15 +1,24 @@
-#include "../../include/utility/chrono.h"
+﻿#include "../../include/utility/chrono.h"
 
 #include <chrono>
 #include <cstdio>
 #include <cstring>
 #include <ctime>
+#include <mutex>
 
 std::string zh::utility::chrono::now()
 {
   enum { NOW_SIZE = 64 };
   static char buffer[NOW_SIZE] { 0 };
   static const char* fmt_now = "%F %T";
+#ifndef _MSC_VER
+  static std::once_flag init_TZ;
+  std::call_once(init_TZ, []{
+    // POSIX 限定：
+    std::string tz = "TZ=Asia/Shanghai";
+    putenv(tz.data());
+  });
+#endif
 
   std::memset(buffer, 0, sizeof buffer);
   const auto t_now = std::chrono::system_clock::to_time_t( \
