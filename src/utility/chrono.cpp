801c6946d15e08035a47618ed28ca7c026a34136
zhangweiguo <27614330@qq.com>
2019-11-20 15:02:00 +0800

    ##

--- src/utility/chrono.cpp
+++ src/utility/chrono.cpp
@@ -10,16 +10,7 @@ std::string zh::utility::chrono::now()
 {
   enum { NOW_SIZE = 64 };
   static char buffer[NOW_SIZE] { 0 };
-  static const char* fmt_now = "%F %T";
-#ifndef _MSC_VER
-  static std::once_flag init_TZ;
-  std::call_once(init_TZ, []{
-    // POSIX 限定：
-    // std::string tz = "TZ=Asia/Shanghai";
-    std::string tz = "TZ=UTC+8:00:00";
-    putenv(tz.data());
-  });
-#endif
+  static const char* fmt_now = "%F %T %z";
 
   std::memset(buffer, 0, sizeof buffer);
   const auto t_now = std::chrono::system_clock::to_time_t( \
