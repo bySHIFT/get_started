68d6d846e4fdb73eefcbdc2fec34e15e490693e5
zhangweiguo <27614330@qq.com>
2020-05-08 20:26:07 +0800

    ##

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "include/theapp.my_request.h"
 #include "include/utility.print.h"
 
+#if defined(_WIN32)
+  #include <windows.h>
+#endif
+
 #include <cstdlib>
 #include <iostream>
 #include <string>
@@ -14,11 +18,23 @@ void print_banner() {
 }
 
 int main() noexcept try {
+#if defined(_WIN32)
+  const auto init_console_cp = GetConsoleCP();
+  const auto init_console_output_cp = GetConsoleOutputCP();
+
+  SetConsoleCP(CP_UTF8);
+  SetConsoleOutputCP(CP_UTF8);
+#endif
   print_banner();
 
   const auto result_cicd = zh::run_my_request();
   std::cout << ">> EXIT: " << result_cicd
     << std::endl;
+
+#if defined(_WIN32)
+  SetConsoleCP(init_console_cp);
+  SetConsoleOutputCP(init_console_output_cp);
+#endif
 
   return result_cicd;
 } catch (...) {
